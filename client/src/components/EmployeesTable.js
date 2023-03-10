d659b6eaf62a72d32aa40bcc8f88c86d74b3c9c4
jackb14 <jack@26385.xyz>
2023-03-10 12:33:43 +0000

    add ts

--- client/src/components/EmployeesTable.js
+++ client/src/components/EmployeesTable.js
@@ -1,45 +1,52 @@
-import React from 'react';
+import React from "react";
 // import * as apiService from '../apiService.js';
-import { useState, useEffect } from 'react';
-import helper from '../helper';
-import { AiOutlineUserDelete } from 'react-icons/ai';
-import { AiOutlineUserAdd } from 'react-icons/ai';
-import './employeesTable.css';
-
+import { useState, useEffect } from "react";
+import helper from "../helper";
+import { AiOutlineUserDelete } from "react-icons/ai";
+import { AiOutlineUserAdd } from "react-icons/ai";
+import "./employeesTable.css";
 
 const EmployeesTable = ({ employees, setEmployees }) => {
-  const [newEmployee, setNewEmployee] = useState({ name: '', surname: '', email: '' });
-  const URL = 'http://localhost:4000/';
+  const [newEmployee, setNewEmployee] = useState({
+    name: "",
+    surname: "",
+    email: "",
+  });
+  const URL = "http://localhost:4000/";
 
   useEffect(() => {
     fetch(`${URL}employees`)
-      .then(response => response.json())
-      .then(data => setEmployees(helper.sortEmployeesByName(data)))
-      .catch(error => console.error(error));
+      .then((response) => response.json())
+      .then((data) => setEmployees(helper.sortEmployeesByName(data)))
+      .catch((error) => console.error(error));
   }, [setEmployees]);
 
   const handleDelete = (id) => {
     fetch(`${URL}employees/${id}`, {
-      method: 'DELETE',
+      method: "DELETE",
     })
-      .then(() => setEmployees(employees.filter(employee => employee.employee_id !== id)))
-      .catch(error => console.error(error));
+      .then(() =>
+        setEmployees(
+          employees.filter((employee) => employee.employee_id !== id)
+        )
+      )
+      .catch((error) => console.error(error));
   };
 
   const handleAdd = () => {
     fetch(`${URL}employee`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newEmployee),
     })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         let updatedList = [...employees, data];
         setEmployees(helper.sortEmployeesByName(updatedList));
       })
-      .catch(error => console.error(error));
+      .catch((error) => console.error(error));
 
-    setNewEmployee({ name: '', surname: '', email: '' });
+    setNewEmployee({ name: "", surname: "", email: "" });
   };
 
   const handleInputChange = (ev) => {
@@ -49,24 +56,26 @@ const EmployeesTable = ({ employees, setEmployees }) => {
 
   const handleUpdate = (id, field, value) => {
     let updatedEmployees = [...employees];
-    updatedEmployees = updatedEmployees.map(emp => emp.employee_id === id ? { ...emp, [field]: value } : emp);
+    updatedEmployees = updatedEmployees.map((emp) =>
+      emp.employee_id === id ? { ...emp, [field]: value } : emp
+    );
     setEmployees(helper.sortEmployeesByName(updatedEmployees));
   };
 
   const handleSave = (id, field, value) => {
     fetch(`${URL}employee/${id}`, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ [field]: value }),
     })
-      .then(response => response)
-      .catch(error => console.error(error));
+      .then((response) => response)
+      .catch((error) => console.error(error));
   };
 
   return (
-    <table className='emp-table'>
+    <table className="emp-table">
       <thead>
-        <tr className='table-head'>
+        <tr className="table-head">
           <th>Name</th>
           <th>Surname</th>
           <th>Email</th>
@@ -74,51 +83,96 @@ const EmployeesTable = ({ employees, setEmployees }) => {
         </tr>
       </thead>
       <tbody>
-        {employees.map(employee => (
-
+        {employees.map((employee) => (
           <tr key={employee.employee_id}>
             <td>
-              <input type="text" defaultValue={employee.name} className='employee-input'
-                onChange={(ev) => handleUpdate(employee.employee_id, 'name', ev.target.value)}
-                onBlur={(ev) => handleSave(employee.employee_id, 'name', ev.target.value)}
+              <input
+                type="text"
+                defaultValue={employee.name}
+                className="employee-input"
+                onChange={(ev) =>
+                  handleUpdate(employee.employee_id, "name", ev.target.value)
+                }
+                onBlur={(ev) =>
+                  handleSave(employee.employee_id, "name", ev.target.value)
+                }
               />
             </td>
             <td>
-              <input type="text" defaultValue={employee.surname} className='employee-input'
-                onChange={(ev) => handleUpdate(employee.employee_id, 'surname', ev.target.value)}
-                onBlur={(ev) => handleSave(employee.employee_id, 'surname', ev.target.value)}
+              <input
+                type="text"
+                defaultValue={employee.surname}
+                className="employee-input"
+                onChange={(ev) =>
+                  handleUpdate(employee.employee_id, "surname", ev.target.value)
+                }
+                onBlur={(ev) =>
+                  handleSave(employee.employee_id, "surname", ev.target.value)
+                }
               />
             </td>
             <td>
-              <input type="email" defaultValue={employee.email} className='employee-input'
-                onChange={(ev) => handleUpdate(employee.employee_id, 'email', ev.target.value)}
-                onBlur={(ev) => handleSave(employee.employee_id, 'email', ev.target.value)}
+              <input
+                type="email"
+                defaultValue={employee.email}
+                className="employee-input"
+                onChange={(ev) =>
+                  handleUpdate(employee.employee_id, "email", ev.target.value)
+                }
+                onBlur={(ev) =>
+                  handleSave(employee.employee_id, "email", ev.target.value)
+                }
               />
             </td>
             <td>
-              <button className='employee-btn delete-btn' onClick={() => handleDelete(employee.employee_id)}><AiOutlineUserDelete /></button>
+              <button
+                className="employee-btn delete-btn"
+                onClick={() => handleDelete(employee.employee_id)}
+              >
+                <AiOutlineUserDelete />
+              </button>
             </td>
           </tr>
         ))}
 
-        <tr className='add-form'>
+        <tr className="add-form">
           <td>
-            <input className='add-input' type="text" name="name" value={newEmployee.name} onChange={handleInputChange} />
+            <input
+              className="add-input"
+              type="text"
+              name="name"
+              value={newEmployee.name}
+              onChange={handleInputChange}
+            />
           </td>
           <td>
-            <input className='add-input' type="text" name="surname" value={newEmployee.surname} onChange={handleInputChange} />
+            <input
+              className="add-input"
+              type="text"
+              name="surname"
+              value={newEmployee.surname}
+              onChange={handleInputChange}
+            />
           </td>
           <td>
-            <input className='add-input' type="email" name="email" value={newEmployee.email} onChange={handleInputChange} />
+            <input
+              className="add-input"
+              type="email"
+              name="email"
+              value={newEmployee.email}
+              onChange={handleInputChange}
+            />
           </td>
           <td>
-            <button className='employee-btn add-btn' onClick={handleAdd}> <AiOutlineUserAdd /> </button>
+            <button className="employee-btn add-btn" onClick={handleAdd}>
+              {" "}
+              <AiOutlineUserAdd />{" "}
+            </button>
           </td>
         </tr>
       </tbody>
     </table>
   );
 };
-
 
 export default EmployeesTable;
--- client/src/helper.tsx
+++ client/src/helper.tsx
@@ -1,8 +1,8 @@
 import { ShiftTypes, Shifts, Employees } from './types';
 const sortShiftTypeByName = (x: ShiftTypes[]) => x.sort((a, b) => a.description.localeCompare(b.description))
 
-export const sortShiftByDate = (x: Shifts[]) => x.sort((a, b) => a.day_number - b.day_number)
+const sortShiftByDate = (x: Shifts[]) => x.sort((a, b) => a.day_number - b.day_number)
 
-export const sortEmployeesByName = (x: Employees[]) => x.sort((a, b) => a.name.localeCompare(b.name))
+const sortEmployeesByName = (x: Employees[]) => x.sort((a, b) => a.name.localeCompare(b.name))
 
-export default {sortShiftTypeByName}
+export default {sortShiftTypeByName, sortShiftByDate, sortEmployeesByName}
