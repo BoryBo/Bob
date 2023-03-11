c655726e35f487e8fc31c8ba3d7feaa39261482e
jackb14 <jack@26385.xyz>
2023-03-11 11:35:44 +0000

    change unique keys to crypto uuid

--- client/src/components/rota/PersonRow.tsx
+++ client/src/components/rota/PersonRow.tsx
@@ -1,27 +1,38 @@
 import PersonShift from "./PersonShift";
 import { Abbreviations, Employee } from "../../types";
 
-function PersonRow({ employee, abbreviations }: { employee: Employee, abbreviations: Abbreviations }) {
-  const days = [...Array(28).keys()].map(x => x + 1).map(x => {
-    const shiftOnDay = employee.shifts.filter((y: any) => y['shifts.day_number'] === x);
-    return shiftOnDay.length > 0 ? shiftOnDay[0] : undefined;
-  });
+function PersonRow({
+  employee,
+  abbreviations,
+}: {
+  employee: Employee;
+  abbreviations: Abbreviations;
+}) {
+  const days = [...Array(28).keys()]
+    .map((x) => x + 1)
+    .map((x) => {
+      const shiftOnDay = employee.shifts.filter(
+        (y: any) => y["shifts.day_number"] === x
+      );
+      return shiftOnDay.length > 0 ? shiftOnDay[0] : undefined;
+    });
 
   return (
     <>
       <div className="rota-row-header">
-        <p >{employee.name}</p>
-        <p className="tot-hours">h: {(Number(employee.hours.toFixed(2)))}</p>
+        <p>{employee.name}</p>
+        <p className="tot-hours">h: {Number(employee.hours.toFixed(2))}</p>
       </div>
-       {days.map(day  => {
-        return day ?
+      {days.map((day) => {
+        return day ? (
           <PersonShift
             abbreviations={abbreviations}
-            key={day.shifts?.[0]?.shift_id ?? Math.floor(Math.random() * 100000)}
+            key={day.shifts?.[0]?.shift_id ?? crypto.randomUUID()}
             shift={day}
           />
-          :
-          <p className="shift" key={Math.floor(Math.random() * 100000)}></p>
+        ) : (
+          <p className="shift" key={crypto.randomUUID()}></p>
+        );
       })}
     </>
   );
